573c0d2b2f491f8bbddfd7e95d6670dd71d0bb4d
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2016-04-28 18:51:44 +0900

    fix

--- exercise/8_1-3.ml
+++ exercise/8_1-3.ml
@@ -8,7 +8,7 @@ let okazukai_rec1 = {name="ペン"; price=110; place="セブンイレブン"; da
 let okazukai_rec2 = {name="幕内弁当"; price=550; place="イトーヨーカドー"; date="2016-04-27"}
 let okazukai_rec3 = {name="あんぱん"; price=100; place="セブンイレブン"; date="2016-04-26"}
 
-type person_t = {name:string; height:float; weight:float; date_of_bairth:string}
-let person1 = {name="山田"; height=175.0; weight=71.;date_of_bairth="1975-01-01"}
-let person2 = {name="岩城"; height=185.0; weight=102.;date_of_bairth="1975-02-03"}
-let person3 = {name="殿間"; height=168.0; weight=65.;date_of_bairth="1975-02-04"}
+type person_t = {name:string; height:float; weight:float; month:int; day:int;}
+let person1 = {name="山田"; height=1.75; weight=71.; month=1; day=21;}
+let person2 = {name="岩城"; height=1.85; weight=102.; month=2; day=22;}
+let person3 = {name="殿間"; height=1.68; weight=65.; month=3; day=23;}
