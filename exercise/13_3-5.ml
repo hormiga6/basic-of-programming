3638ade1e56397ad6aca86de58716704ad2aeeef
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2016-12-22 20:09:40 +0900

    add

--- exercise/13_3-5.ml
+++ exercise/13_3-5.ml
@@ -12,3 +12,22 @@ let fun4 a b = (b a)
 
 (* 5 *)
 let fun5 f1 f2 v = f2 (f1 v)
+
+(* 目的: 関数を二つ受け取ったら合成した関数を返す関数
+	(comopse time2 add3) 4は　2 * (3 + 4) = 14
+*)
+let comopse f1 f2 =
+	let f a = (f1 (f2 a)) in
+	f
+let time2 num =  num * 2
+let add3 num = num + 3
+
+let t1 = (comopse time2 add3) 4 = 14
+
+(* twice *)
+let twice f =
+	let g x = f (f x)
+	in g
+let t2 = twice twice
+let t3 = twice twice add3 1 = 13
+
